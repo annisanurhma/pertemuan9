8b294f66f9fcc95cec4c7d4dd4db4a335b3c9845
annisanurhma <nurrohmaanisa50@gmail.com>
2025-11-19 11:24:56 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,6 @@
+#nomor 1
+def celcius_ke_fahrenheit(celcius):
+    return (celcius * 9/5) + 32
+
+print(celcius_ke_fahrenheit(0))    # Output: 32.0
+print(celcius_ke_fahrenheit(100))  # Output: 212.0
---
+++ soal2.py
@@ -0,0 +1,4 @@
+#nomor 2
+def is_genap(n):
+    return n % 2 == 0
+print (is_genap(2))
